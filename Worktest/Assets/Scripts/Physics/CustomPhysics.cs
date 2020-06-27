327dc3d82aae149bacb4eef4e098c7baa1f90158
esteban127 <esteban_127@hotmail.com>
2020-06-27 18:03:58 -0300

    set pyhisic basics

---
+++ Worktest/Assets/Scripts/Physics/CustomPhysics.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPhysics
+{
+    Dictionary<string, BoxCollider2D> collisionAreas;
+
+
+    static private CustomPhysics instance = null;
+
+    static public CustomPhysics Instance 
+    { get 
+        { 
+        if(instance == null){
+            instance = new CustomPhysics();
+            }
+        return instance; 
+        }
+    }
+
+    public void UpdateCollision(string key, BoxCollider2D area)
+    {
+        if (collisionAreas.ContainsKey(key))
+        {
+            collisionAreas[key] = area;
+        }
+        else
+        {
+            collisionAreas.Add(key, area);
+        }
+    }
+
+    public void RemoveCollision(string key)
+    {
+        if (collisionAreas.ContainsKey(key))
+        {
+            collisionAreas.Remove(key);
+        }
+    }
+
+    public List<BoxCollider2D> GetCollisions()
+    {
+        List<BoxCollider2D> areas = new List<BoxCollider2D>();
+        foreach(string key in collisionAreas.Keys)
+        {
+            areas.Add(collisionAreas[key]);
+        }
+        return areas;
+    }
+
+}
---
+++ Worktest/Assets/Scripts/Physics/RockPhysics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPhysics : MonoBehaviour
+{
+    Vector2 moveVector;
+    [SerializeField]
+    float gravity = 9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = new Vector2();
+        moveVector += gravity * Vector2.down;
+        transform.position= transform.position + (new Vector3(moveVector.x , moveVector.y,0) * Time.deltaTime);
+    }
+}
