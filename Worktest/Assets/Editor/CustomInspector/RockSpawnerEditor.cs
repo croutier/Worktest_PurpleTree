07f974ca9827c2fd7caa4c5543a47a244c918f5c
esteban127 <esteban_127@hotmail.com>
2020-06-29 17:07:32 -0300

    RockSawpner Inspector

---
+++ Worktest/Assets/Editor/CustomInspector/RockSpawnerEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+    using UnityEditor;
+
+[CustomEditor(typeof(RockSpawner))]
+public class RockSpawnerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        RockSpawner myScript = (RockSpawner)target;
+        SerializedObject serializedObj = new SerializedObject(myScript);
+
+        DrawPropertiesExcluding(serializedObj, new string[] { "minThrowAngle", "maxThrowAngle", "minStrength", "maxStrength", "minTimeBetweenSpawns", "maxTimeBetweenSpawns" });
+        GUILayout.Label("");
+        GUIStyle customGUIStyle = new GUIStyle();
+        customGUIStyle.fontSize = 16;
+        EditorGUILayout.LabelField("Angle",style: customGUIStyle);       
+
+        EditorGUILayout.LabelField("Min:", myScript.minThrowAngle.ToString("#.#") +"°");
+        EditorGUILayout.LabelField("Max:", myScript.maxThrowAngle.ToString("#.#") + "°");
+        EditorGUILayout.MinMaxSlider(ref myScript.minThrowAngle, ref myScript.maxThrowAngle, 0.0f, 90);
+
+        EditorGUILayout.LabelField("Strength", style: customGUIStyle);
+        EditorGUILayout.LabelField("Min:", myScript.minStrength.ToString("#.#"));
+        EditorGUILayout.LabelField("Max:", myScript.maxStrength.ToString("#.#"));
+        EditorGUILayout.MinMaxSlider(ref myScript.minStrength, ref myScript.maxStrength, 1, 20);
+
+        EditorGUILayout.LabelField("Time between spawns", style: customGUIStyle);
+        EditorGUILayout.LabelField("Min:", myScript.minTimeBetweenSpawns.ToString("#.#") +" s");
+        EditorGUILayout.LabelField("Max:", myScript.maxTimeBetweenSpawns.ToString("#.#") + " s");
+        EditorGUILayout.MinMaxSlider(ref myScript.minTimeBetweenSpawns, ref myScript.maxTimeBetweenSpawns, 1, 30);
+
+        serializedObj.ApplyModifiedProperties();
+    }
+}
+#endif
--- Worktest/Assets/Scripts/Enemy/RockSpawner.cs
+++ Worktest/Assets/Scripts/Enemy/RockSpawner.cs
@@ -9,8 +9,30 @@ public class RockSpawner : MonoBehaviour
     [SerializeField]
     GameObject goal;
 
-    public void SpawnRocks()
+    public float minThrowAngle = 30.0f;    
+    public float maxThrowAngle = 50.0f;
+    public float minStrength = 1;
+    public float maxStrength = 8;
+    public float minTimeBetweenSpawns = 5;
+    public float maxTimeBetweenSpawns = 15;
+
+    float nextSpawnTime = 3.0f;
+
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+
+    private void Update()
+    {
+        
+    }
+
+    public void SpawnRock()
     {
 
     }
+    
 }
